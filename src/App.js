2df8c36b8aad2b8069d60f9bf56f709a0bf9314e
Philip Loa <boyet007@gmail.com>
2019-10-16 15:32:32 +0700

    CRUD Completed todos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
+
+
 
 function App() {
+  const [todos, setTodos] = useState([
+    { text: 'Learn about React', inCompleted: false },
+    { text: 'Meet friends for lunch', inCompleted: false },
+    { text: 'Build really cool app todo', inCompleted: false }
+  ])
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }]
+    setTodos(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].isCompleted = true
+    setTodos(newTodos)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card-content">
+        { todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo}
+             completeTodo={completeTodo} removeTodo={removeTodo}/>
+        )) }
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
